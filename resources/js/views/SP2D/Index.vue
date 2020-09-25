9518b0e1f0637345ec516f1a03d1732abce586ab
Riky Hasibuan <riky.hasibuan@gmail.com>
2020-09-25 14:52:04 +0700

    SP2D Fix Index

--- resources/js/views/SP2D/Index.vue
+++ resources/js/views/SP2D/Index.vue
@@ -77,7 +77,7 @@
                                             <th style="width:10%;text-align:center;vertical-align: middle;" colspan="4">Nilai SP2D</th>
                                             <th style="width:10%;text-align:center;vertical-align: middle;" rowspan="2">Jumlah SP2D</th>
                                             <th style="width:10%;text-align:center;vertical-align: middle;" rowspan="2">Sisa Anggaran</th>
-                                            <th style="width:12%;text-align:center;vertical-align: middle;" rowspan="2">Action</th>
+                                            <th style="width:8%;text-align:center;vertical-align: middle;" rowspan="2">Action</th>
                                         </tr>
                                         <tr>
                                             <th style="width:10%;text-align:center;vertical-align: middle;">UP</th>
@@ -114,29 +114,29 @@
                                             </td>
                                             <td style="text-align: center;vertical-align: middle;">
                                                 <div>
-                                                    <a 
-                                                        v-if="(access.update === 1)" 
-                                                        :href="route + '/edit?id=' + v.id" 
+                                                    <a
+                                                        v-if="(access.update === 1)"
+                                                        :href="route + '/edit?id=' + v.id"
                                                         class="btn btn-sm btn-warning mr-sm-1">
                                                         <i class="fa fa-wrench"></i> Ubah
                                                     </a>
-                                                    <button 
-                                                        v-else 
+                                                    <button
+                                                        v-else
                                                         class="btn btn-sm btn-warning disabled mr-sm-1">
                                                         <i class="fa fa-wrench"></i> Ubah
                                                     </button>
-                                                    <a 
-                                                        v-if="(access.delete === 1)" 
-                                                        href="#" 
+                                                    <!-- <a
+                                                        v-if="(access.delete === 1)"
+                                                        href="#"
                                                         @click="toggleModal(v.id)"
                                                         class="btn btn-sm btn-danger">
                                                         <i class="fa fa-trash-o"></i> Hapus
                                                     </a>
-                                                    <button 
-                                                        v-else 
+                                                    <button
+                                                        v-else
                                                         class="btn btn-sm btn-danger disabled">
                                                         <i class="fa fa-trash-o"></i> Hapus
-                                                    </button>
+                                                    </button> -->
                                                 </div>
                                             </td>
                                         </tr>
